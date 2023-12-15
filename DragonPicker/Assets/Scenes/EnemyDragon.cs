e386f1ff0a7d0d2246ec2cafd9bf9bb765a14eee
v1ad11k <146327840+v1ad11k@users.noreply.github.com>
2023-12-15 20:00:16 +0500

    1-2

---
+++ DragonPicker/Assets/Scenes/EnemyDragon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyDragon : MonoBehaviour
+{
+
+    public GameObject dragonEggPrefab; // создаЄт префаб дл€ ийца
+    public float speed = 1f; // определ€ет скорость движени€ объекта
+    public float timeBetweenEggDrops = 1f; // определ€ет скорость генерации €иц
+    public float leftRightDistance = 10f; // определ€ет рассто€ние от левых и правых краев экрана на котором мен€етс€ направление движени€ дракона
+    public float chanceDirections = 0.1f; // ќпредел€ет веро€тность изменени€ направлени€ движени€
+    void Start()
+    {
+    }
+    void Update()
+    {
+        Vector3 pos = transform.position; //1
+        pos.x += speed * Time.deltaTime; //2
+        transform.position = pos; //3
+
+        if (pos.x < -leftRightDistance) // условие , чтобы дракон не улетал за кра€
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftRightDistance) 
+        {
+            speed = -Mathf.Abs(speed);
+        }
+
+    }
+
+    private void FixedUpdate() // метод случайного направлени€ движени€
+    {
+        if (Random.value < chanceDirections)
+        {
+            speed *= -1;
+        }
+    }
+
+}
